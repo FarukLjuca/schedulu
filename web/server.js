df15ba14277e4f85c2d0256828515d78a4a6df71
FarukLjuca <echo.faruk@gmail.com>
2017-09-03 22:34:35 +0200

    Finished sending messages with test token and postman

---
+++ web/server.js
@@ -0,0 +1,32 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var fetch = require('node-fetch')
+var FormData = require('form-data')
+
+var app = express()
+var port = 8000
+
+app.use(bodyParser.json())
+
+app.post('/send', (req, res) => {
+
+  var token = ''
+  var channel = 'D613S2GP9'
+  var text = req.body.text
+
+  fetch('https://slack.com/api/chat.postMessage?token=' + token + '&channel=' + channel + '&text=' + text)
+    .then(response => {
+      if (response.status == 200) {
+        res.send('Message sent successfully')
+      } else {
+        res.send('There was error sending message')
+      }
+    })
+    .catch(error => {
+      res.send(error)
+    })
+})
+
+app.listen(port, () => {
+  console.log('App is listening to port ' + port)
+})
